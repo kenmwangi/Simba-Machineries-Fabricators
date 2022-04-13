e22a32e4582dd8c30c45df3c468441c3495fdf3f
Ken Mwangi <kenmwangi071@gmail.com>
2022-04-13 14:46:13 +0300

    Fixed React 18 dependency bugs

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -1,4 +1,4 @@
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+// import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { createClient } from "contentful";
 import Image from "next/image";
 import React from "react";
@@ -58,7 +58,7 @@ function ProductDetail({ equipment }) {
         </div> */}
         <div className={style.description}>
           <h3>Description</h3>
-          <div>{documentToReactComponents(description)}</div>
+          {/* <div>{documentToReactComponents(description)}</div> */}
         </div>
       </div>
     </section>
